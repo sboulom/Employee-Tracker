61a1084e2a235c66989d9a9423d9be989822a9fc
Steven Boulom <sboulom@gmail.com>
2020-03-24 14:25:21 -0700

    updated schema, corrected seeds and added 2 functions

---
+++ employee.js
@@ -0,0 +1,105 @@
+var inquirer = require('inquirer')
+var mysql = require('mysql')
+require('console.table')
+var connection = mysql.createConnection({
+  host: 'localhost',
+  port: 3306,
+  user: 'root',
+  password: 'passwordpassword',
+  database: 'employee_db'
+})
+
+connection.connect(function(err) {
+  console.log('connection id' + connection.threadId)
+  start()
+})
+var viewAllEmp = 'View all employees'
+var viewByDept = 'View all employees by department'
+var viewAllRoles = 'View all roles'
+var addEmp = 'Add Employee'
+var removeEmp = 'Remove Employee'
+var updateEmp = 'Update Employee Role'
+
+function start() {
+  inquirer
+    .prompt({
+      type: 'list',
+      message: 'Please choose a selection',
+      choices: [
+        viewAllMessage,
+        viewByDept,
+        viewEmpByManager,
+        addEmp,
+        removeEmp,
+        updateEmp
+      ],
+      name: 'employee'
+    })
+    .then(function(userinput) {
+      console.log(userinput.employee)
+      switch (userinput.employee) {
+        case viewAllEmp:
+          viewAllEmployee()
+          break
+        case viewByDept:
+          viewAllDepartments()
+          break
+        case viewByRole:
+          viewAllRoles()
+          break
+        default:
+          console.log('ended on default')
+      }
+    })
+}
+function viewAllEmployee() {
+  connection.query(
+    `SELECT employee.first_name, employee.last_name, role.title, employee.role_id, department.dept_name, salary, concat( employee1.first_name, ' ', employee1.last_name)
+      manager, employee1.manager_id
+      FROM employee 
+      LEFT JOIN role on employee.role_id= role.id
+      LEFT JOIN department on department.id = role.department_id
+      LEFT JOIN employee employee1 on employee1.id = employee.manager_id`,
+    function(err, data) {
+      //   console.log(data)
+      console.table(data)
+    }
+  )
+}
+// function empByDept(departmentID) {
+//   connection.query(
+//     `SELECT employee.first_name, employee.last_name, department.dept_name
+//         FROM employee
+//         LEFT JOIN role on employee.role_id = role.id
+//         LEFT JOIN department on department.id = role.department_id
+//         WHERE department.id = ?`,
+//     departmentID,
+//     function(err, data) {
+//       console.log(data)
+//       console.table(data)
+//     }
+//   )
+//   console.log(empByDept)
+// }
+
+function viewAllDepartments() {
+  connection.query(
+    `SELECT dept_name
+        FROM department`,
+    function(err, data) {
+      //   console.log(data)
+      console.table(data)
+    }
+  )
+}
+
+function viewAllRoles() {
+  connection.query(
+    `SELECT title
+        FROM role`,
+    function(err, data) {
+      //   console.log(data)
+      console.table(data)
+    }
+  )
+}
--- schema.sql
+++ schema.sql
@@ -6,24 +6,27 @@ USE employee_db;
 
 CREATE TABLE employee
 (
-
+     id INTEGER NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INTEGER(20),
-    manager_ID INTEGER(20)
+    manager_ID INTEGER(20) NULL,
+    PRIMARY KEY (id)
 
 );
 
 CREATE TABLE role
 (
-
+   id INTEGER NOT NULL AUTO_INCREMENT,
     title VARCHAR(30) NOT NULL,
     salary DECIMAL (10,3) NULL,
     department_id INTEGER(20),
-
+	PRIMARY KEY (id)
 );
 
 CREATE TABLE department
 (
+	id INTEGER NOT NULL AUTO_INCREMENT,
     dept_name VARCHAR (30) NOT NULL,
+    PRIMARY KEY (id)
 )
